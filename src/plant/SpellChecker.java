cbb36333e3fecc255ae9e60e985f23f8a914cd97
Bryan Plant <bryanplant@gmail.com>
2017-04-26 21:04:20 -0600

    Need to put periods/caps back in still

--- src/plant/SpellChecker.java
+++ src/plant/SpellChecker.java
@@ -1,14 +1,19 @@
 package plant;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Scanner;
 
+import edu.princeton.cs.algs4.MaxPQ;
 import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.ST;
 import edu.princeton.cs.algs4.TST;
 
 public class SpellChecker {
@@ -27,22 +32,24 @@ public class SpellChecker {
 			sp.getDictionary(args[0]);
 		}
 		catch(IOException e){
-			System.err.println("Check filepath and text file");
+			System.err.println("Check filepaths and text files");
 		}
 
-		String word = "doog";
-		ArrayList<String> list = sp.checkWord(word);
-		if(list != null){
-			System.out.println(word + ": Did you mean");
-			for(int i = 0; i < list.size(); i++){
-				System.out.println("\t" + (i+1) + ": " + list.get(i));
-			}
+		try{
+			sp.read(args[1]);
 		}
+		catch(IOException e){
+			System.err.println("Check filepaths and text files");
+		}
+
+
+		sp.keyboardDistance('b', 'g');
+		sp.keyboardDistance('b', 'j');
 	}
 
 	public SpellChecker(){
 		tst = new TST<Integer>();
-		keyboard = new char[][]{{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'}, {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\''}, {'z', 'x', 'c', 'v', 'b', 'n', 'm'}};
+		keyboard = new char[][]{{'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'}, {'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', '\''}, {'z', 'x', 'c', 'v', 'b', 'n', 'm', ' ', ' ', ' '}};
 	}
 
 	public void getDictionary(String filename) throws IOException{
@@ -61,36 +68,112 @@ public class SpellChecker {
 				i++;
 			}
 		}
+		br.close();
+	}
+
+	public void read(String filename) throws IOException{
+		Scanner sc = null;
+		try{
+			sc = new Scanner(new FileReader(filename));
+		} catch(FileNotFoundException e){
+			throw new IOException();
+		}
+
+
+
+		String corrected = "";
+		while(sc.hasNextLine()){
+			Scanner sc2 = new Scanner(sc.nextLine());
+			Scanner in = new Scanner(System.in);
+			String word;
+			while(sc2.hasNext()){
+				word = sc2.next();
+
+				ArrayList<String> list = checkWord(word);
+				if (list == null){}
+				else{
+					int i = 0;
+					System.out.println(word + ": Did you mean");
+					for(i = 0; i < list.size(); i++){
+						System.out.println("\t" + (i+1) + ": " + list.get(i));
+					}
+					System.out.println("\t" + (i+1) + ": " + "somthing else");
+					boolean valid = false;
+					while(!valid){
+						try{
+							word = list.get(Integer.parseInt(in.next())-1);
+							valid = true;
+						}
+						catch(Exception e){
+							System.out.println("Enter a number between 1 & " + (list.size()+1));
+						}
+					}
+				}
+				corrected = corrected.concat(word + " ");
+			}
+		}
+
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+
+		try{
+			fw = new FileWriter("mydoc-checked.txt");
+			bw = new BufferedWriter(fw);
+		}
+		catch(IOException e){
+			sc.close();
+			throw new IOException();
+		}
+		bw.write(corrected);
+		bw.close();
+
+		sc.close();
 	}
 
 	public ArrayList<String> checkWord(String word){
 		word = word.toLowerCase();
+		StringBuilder sb = new StringBuilder(word);
+		if(sb.indexOf(".") != -1)
+			sb.deleteCharAt(sb.indexOf("."));
+		word = sb.toString();
 
 		if(tst.contains(word)){
 			return null;
 		}
 
-		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> list = new ArrayList<>();
 		for(int i = 0; i < word.length(); i++){
 			char[] letters = word.toCharArray();
 			letters[i] = '.';
 			String newWord = new String(letters);
 			Queue<String> queue = (Queue<String>) tst.keysThatMatch(newWord);
 			for(String s : queue){
-				keyboardDistance(newWord.charAt(i), word.charAt(i));
+				//double distance = keyboardDistance(newWord.charAt(i), word.charAt(i));
 				list.add(s);
 			}
 		}
 		return list;
 	}
 
-	public void keyboardDistance(char char1, char char2){
-		int x1, y1, x2, y2;
-		for(x1 = 0; x1 < keyboard.length; x1++){
-			for(y1 = 0; y1 < keyboard[0].length; y1++){
-				if(keyboard[x1][y1] == char1)
-					break;
+	public double keyboardDistance(char char1, char char2){
+		int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+		for(int i = 0; i < keyboard.length; i++){
+			for(int j = 0; j < keyboard[0].length; j++){
+				if(keyboard[i][j] == char1){
+					x1 = i;
+					y1 = j;
+				}
 			}
 		}
+		for(int i = 0; i < keyboard.length; i++){
+			for(int j = 0; j < keyboard[0].length; j++){
+				if(keyboard[i][j] == char2){
+					x2 = i;
+					y2 = j;
+				}
+			}
+		}
+		double distance = Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		return distance;
 	}
 }
